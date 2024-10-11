3c4c502d0f183217535aa6189bb661a96c9b5c86
Brian West <bwest@xometry.com>
2024-10-11 11:33:27 -0400

    test find_hidden_triples()

--- rust/sudoku/src/sudoku.rs
+++ rust/sudoku/src/sudoku.rs
@@ -3055,7 +3055,6 @@ mod tests {
     #[test]
     fn test_find_hidden_pairs_row() {
         // hidden pair (2 7) at cell(3, 4) and cell(3, 6), for row 3
-        // found naked pair (2, 6) for row 3
         let mut puzzle: SudokuPuzzle = create_empty_sudoku_puzzle();
 
         // candidates 2 and 7 in row 3
@@ -3163,16 +3162,136 @@ mod tests {
 
     #[test]
     fn test_find_hidden_triples_row() {
-        assert!(true);
+        // found hidden triple (1 2 3) at cell(3, 1) and cell(3, 4) and cell(3, 9), for row 3
+        //     (1 2)   in cell(3, 1)
+        //     (2 3)   in cell(3, 4)
+        //     (1 2 3) in cell(3, 9)
+        let mut puzzle: SudokuPuzzle = create_empty_sudoku_puzzle();
+
+        // candidates 1, 2 and 3 in row 3
+        let candidate1: usize = 0;
+        let candidate2: usize = 1;
+        let candidate3: usize = 2;
+        let row: usize = 2;
+        let column1: usize = 0;
+        let column2: usize = 3;
+        let column3: usize = 8;
+
+        // for row 3, all non-candidate cells need to exclude candidates
+        for j in 0..9 {
+            if j != column1 && j != column2 && j != column3 {
+                puzzle.cells[row][j].set_possible(candidate1, false);
+                puzzle.cells[row][j].set_possible(candidate2, false);
+                puzzle.cells[row][j].set_possible(candidate3, false);
+            }
+        }
+
+        let found = puzzle.find_hidden_triples();
+        assert_eq!(found, true);
+
+        // cell(3, 1), cell(3, 4) and cell(3, 9) need to exclude all non-candidates
+        for k in 0..9 {
+            if k != candidate1 && k != candidate2 && k != candidate3 {
+                assert_eq!(puzzle.cells[row][column1].is_possible(k), false);
+                assert_eq!(puzzle.cells[row][column2].is_possible(k), false);
+                assert_eq!(puzzle.cells[row][column3].is_possible(k), false);
+            }
+        }
     }
 
     #[test]
     fn test_find_hidden_triples_column() {
-        assert!(true);
+        // found hidden triple (1 2 3) at cell(1, 3) and cell(4, 3) and cell(9, 3), for column 3
+        //     (1 2)   in cell(1, 3)
+        //     (2 3)   in cell(4, 3)
+        //     (1 2 3) in cell(9, 3)
+        let mut puzzle: SudokuPuzzle = create_empty_sudoku_puzzle();
+
+        // candidates 1, 2 and 3 in column 3
+        let candidate1: usize = 0;
+        let candidate2: usize = 1;
+        let candidate3: usize = 2;
+        let row1: usize = 0;
+        let row2: usize = 3;
+        let row3: usize = 8;
+        let column: usize = 2;
+
+        // for column 3, all non-candidate cells need to exclude candidates
+        for i in 0..9 {
+            if i != row1 && i != row2 && i != row3 {
+                puzzle.cells[i][column].set_possible(candidate1, false);
+                puzzle.cells[i][column].set_possible(candidate2, false);
+                puzzle.cells[i][column].set_possible(candidate3, false);
+            }
+        }
+
+        let found = puzzle.find_hidden_triples();
+        assert_eq!(found, true);
+
+        // cell(3, 1), cell(3, 4) and cell(3, 9) need to exclude all non-candidates
+        for k in 0..9 {
+            if k != candidate1 && k != candidate2 && k != candidate3 {
+                assert_eq!(puzzle.cells[row1][column].is_possible(k), false);
+                assert_eq!(puzzle.cells[row2][column].is_possible(k), false);
+                assert_eq!(puzzle.cells[row3][column].is_possible(k), false);
+            }
+        }
     }
 
     #[test]
     fn test_find_hidden_triples_grid() {
-        assert!(true);
+        // found hidden triple (1 2 3) at cell(4, 4) and cell(5, 5) and cell(6, 6), for grid(2, 2)
+        //     (1 2)   in cell(4, 4)
+        //     (2 3)   in cell(5, 5)
+        //     (1 2 3) in cell(6, 6)
+        let mut puzzle: SudokuPuzzle = create_empty_sudoku_puzzle();
+
+        // candidates 1, 2 and 3 in column 3
+        let candidate1: usize = 0;
+        let candidate2: usize = 1;
+        let candidate3: usize = 2;
+        let row1: usize = 3;
+        let row2: usize = 4;
+        let row3: usize = 5;
+        let column1: usize = 3;
+        let column2: usize = 4;
+        let column3: usize = 5;
+
+        // for grid(2, 2), all non-candidate cells need to exclude candidates
+        let gridi_start: usize = (row1 / 3) * 3;
+        let gridj_start: usize = (column1 / 3) * 3;
+        for i in gridi_start..gridi_start+3 {
+            for j in gridj_start..gridj_start+3 {
+                if i == row1 && j == column1 {
+                    puzzle.cells[i][j].set_possible(candidate1, true);
+                    puzzle.cells[i][j].set_possible(candidate2, true);
+                }
+                else if i == row2 && j == column2 {
+                    puzzle.cells[i][j].set_possible(candidate2, true);
+                    puzzle.cells[i][j].set_possible(candidate3, true);
+                }
+                else if i == row3 && j == column3 {
+                    puzzle.cells[i][j].set_possible(candidate1, true);
+                    puzzle.cells[i][j].set_possible(candidate2, true);
+                    puzzle.cells[i][j].set_possible(candidate3, true);
+                }
+                else {
+                    puzzle.cells[i][j].set_possible(candidate1, false);
+                    puzzle.cells[i][j].set_possible(candidate2, false);
+                    puzzle.cells[i][j].set_possible(candidate3, false);
+                }
+            }
+        }
+
+        let found = puzzle.find_hidden_triples();
+        assert_eq!(found, true);
+
+        for k in 0..9 {
+            if k != candidate1 && k != candidate2 && k != candidate3 {
+                assert_eq!(puzzle.cells[row1][column1].is_possible(k), false);
+                assert_eq!(puzzle.cells[row2][column2].is_possible(k), false);
+                assert_eq!(puzzle.cells[row3][column3].is_possible(k), false);
+            }
+        }
     }
 }
