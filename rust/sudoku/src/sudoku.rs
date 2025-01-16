ba3278846b999e59e966b18f721333f641a9e4bd
Brian West <bwest@xometry.com>
2025-01-16 10:06:19 -0500

    comments

--- rust/sudoku/src/sudoku.rs
+++ rust/sudoku/src/sudoku.rs
@@ -2676,6 +2676,19 @@ pub fn solve_puzzle(puzzle: &mut SudokuPuzzle) -> bool {
 // will run the unit tests
 //
 
+//
+// https://doc.rust-lang.org/book/ch11-03-test-organization.html
+//
+// Unit Tests
+//
+// The purpose of unit tests is to test each unit of code in isolation
+// from the rest of the code to quickly pinpoint where code is and isn’t working
+// as expected. You’ll put unit tests in the src directory in each file with the
+// code that they’re testing. The convention is to create a module named tests
+// in each file to contain the test functions and to annotate the module
+// with cfg(test).
+//
+
 #[cfg(test)]
 mod tests {
     // pull outer scope into inner scope.
